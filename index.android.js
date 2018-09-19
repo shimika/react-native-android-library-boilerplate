8d73449c6c83776ad8162edfd8cd02c868d7c318
Mika Jihoon Yoon <mika@zoyi.co>
2018-09-19 15:07:58 +0900

    Create track function

--- index.android.js
+++ index.android.js
@@ -5,9 +5,9 @@ import { NativeModules } from 'react-native'
 // name as defined via ReactContextBaseJavaModule's getName
 
 module.exports = {
-  track: function(pluginkey, userId, profile) {
-    if (!pluginkey) {
-      console.log('pluginkey can not be null or empty')
+  track: function(pluginKey, userId, profile) {
+    if (!pluginKey) {
+      console.log('plugin key can not be null or empty')
       return
     }
     if (!userId) {
@@ -15,7 +15,24 @@ module.exports = {
       return
     }
 
-    console.log(pluginkey, userId, profile)
+    NativeModules.ChannelIOSynergy.getDeviceId(deviceId => {
+      fetch(`https://api.channel.io/app/plugins/${pluginKey}/boot/v2`, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          userId,
+          profile,
+          sysProfile: {
+            '$wId': deviceId
+          }
+        })
+      }).catch((error) => {
+        console.log(error)
+      })
+    })
   },
   ...NativeModules.ChannelIOSynergy
 }
