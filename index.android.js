eec752f7f21f4ea1ee631d693904036fa05b0078
Mika Jihoon Yoon <mika@zoyi.co>
2018-09-19 14:49:57 +0900

    Add module function

--- index.android.js
+++ index.android.js
@@ -1,5 +1,23 @@
 'use strict'
 
 import { NativeModules } from 'react-native'
+
+import AAA from 'blahblah'
+import { a } from 'blahblah'
 // name as defined via ReactContextBaseJavaModule's getName
-module.exports = NativeModules.ChannelIOSynergy
+
+module.exports = {
+  track: function(pluginkey, userId, profile) {
+    if (!pluginkey) {
+      console.log('pluginkey can not be null or empty')
+      return
+    }
+    if (!userId) {
+      console.log('userId can not be null or empty')
+      return
+    }
+
+    console.log(pluginkey, userId, profile)
+  },
+  ...NativeModules.ChannelIOSynergy
+}
