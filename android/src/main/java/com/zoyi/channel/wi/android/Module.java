efe3dcfdd25d63a075407ca329f8ddbb095b62e6
Mika Jihoon Yoon <mika@zoyi.co>
2018-09-19 15:25:39 +0900

    Remove library

--- android/src/main/java/com/zoyi/channel/wi/android/Module.java
+++ android/src/main/java/com/zoyi/channel/wi/android/Module.java
@@ -1,16 +1,9 @@
 package com.zoyi.channel.wi.android;
 
-import android.util.Log;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import rx.Subscriber;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.schedulers.Schedulers;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class Module extends ReactContextBaseJavaModule {
 
@@ -26,97 +19,8 @@ public class Module extends ReactContextBaseJavaModule {
   }
 
   @ReactMethod
-  public void setDebug(boolean debug) {
-    this.debug = debug;
-  }
-
-  @ReactMethod
   public void getDeviceId(Promise promise) {
     promise.resolve(Utils.getWId());
   }
 
-  @ReactMethod
-  public void version() {
-    ChannelApiFactory.getApi().version()
-        .subscribeOn(Schedulers.io())
-        .observeOn(AndroidSchedulers.mainThread())
-        .subscribe(new Subscriber<Void>() {
-          @Override
-          public void onCompleted() {
-
-          }
-
-          @Override
-          public void onError(Throwable e) {
-
-          }
-
-          @Override
-          public void onNext(Void aVoid) {
-
-          }
-        });
-  }
-
-  @ReactMethod
-  public void boot(String pluginKey, String id, String age, String gender) {
-    if (pluginKey == null || pluginKey.trim().length() == 0) {
-      Log.e("Channel", "Plugin key can not be null or empty");
-      return;
-    }
-    if (id == null || id.trim().length() == 0) {
-      Log.e("Channel", "ID can not be null or empty");
-      return;
-    }
-
-    Map<String, Object> form = new HashMap<>();
-
-    form.put("userId", id);
-
-    if (age != null || gender != null) {
-      Map<String, String> profileMap = new HashMap<>();
-
-      if (age != null) {
-        profileMap.put("age", age);
-      }
-      if (gender != null) {
-        profileMap.put("gender", gender);
-      }
-
-      form.put("profile", profileMap);
-    }
-
-    String wId = Utils.getWId();
-    if (wId != null) {
-      Map<String, String> sysProfileMap = new HashMap<>();
-
-      sysProfileMap.put("$wId", wId);
-
-      form.put("sysProfile", sysProfileMap);
-    }
-
-    ChannelApiFactory.getApi().bootV2(pluginKey, Utils.form(form))
-        .subscribeOn(Schedulers.io())
-        .observeOn(AndroidSchedulers.mainThread())
-        .subscribe(new Subscriber<Void>() {
-          @Override
-          public void onCompleted() {
-          }
-
-          @Override
-          public void onError(Throwable throwable) {
-            if (throwable != null) {
-              Log.e("Channel", "Failed: " + throwable.getMessage());
-              throwable.printStackTrace();
-            } else {
-              Log.e("Channel", "Failed");
-            }
-          }
-
-          @Override
-          public void onNext(Void aVoid) {
-            Log.d("Channel", "Success");
-          }
-        });
-  }
 }
--- android/src/main/java/com/zoyi/channel/wi/android/Utils.java
+++ android/src/main/java/com/zoyi/channel/wi/android/Utils.java
@@ -1,16 +1,10 @@
 package com.zoyi.channel.wi.android;
 
 import android.annotation.SuppressLint;
-import android.content.Context;
-import okhttp3.RequestBody;
-import org.json.JSONObject;
 
 import java.net.NetworkInterface;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
-
-import static okhttp3.MediaType.parse;
 
 /**
  * Created by mika on 2018. 9. 18..
@@ -45,9 +39,4 @@ public class Utils {
     return null;
   }
 
-  public static RequestBody form(Map<String, Object> map) {
-    return RequestBody.create(
-        parse("application/json; charset=utf-8"),
-        new JSONObject(map).toString());
-  }
 }
